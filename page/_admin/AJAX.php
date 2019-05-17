7f14bdc82e98b417174571256c2bbc3f5ba61c36
DEEP THOUGHT v2 [EARTH] <DEEP@THOUGHTS.question>
2019-05-17 03:40:31 +0200

    trying to upload a file..

--- page/_admin/AJAX.php
+++ page/_admin/AJAX.php
@@ -56,12 +56,65 @@ function saveBlogDB()
 	// TODO: save blog db.
 }
 
+// returns the new filename on success, -1 on fail.
+function phpupload($fileID)
+{
+	global $langDB;
+	//global $sentence_new_page_created;
+	global $dirToRoot;
+	global $relative_upload_path;
+
+	$errcode="";
+	$filename=$_FILES[$fileID]['name'];
+	switch($_FILES[$fileID]['error'])
+	{
+       		case UPLOAD_ERR_OK:
+        	   	break;
+        	case UPLOAD_ERR_NO_FILE:
+        	    $errcode= '[PHP] No file sent.<br />';
+				break;
+        	case UPLOAD_ERR_INI_SIZE:
+        	case UPLOAD_ERR_FORM_SIZE:
+        	    $errcode='[PHP] Exceeded filesize limit.<br />';
+				break;
+        	default:
+        	    $errcode= '[PHP] Unknown errors.<br />';
+				break;
+	}
+
+	if($errcode=="")
+	{
+		if(isset($_FILES[$fileID]['tmp_name']))
+		{
+			if(!empty($filename))
+			{
+				$newfilename=time()."_".$filename;
+				if(!move_uploaded_file($_FILES[$fileID]['tmp_name'],$dirToRoot.$relative_upload_path.$newfilename))
+				{
+					$errcode="[PHP] Move file failed.<br />";
+				}else{
+					return $newfilename;
+				}
+			}else{
+				$errcode="[PHP] No Filename.<br />";
+			}
+		}
+	}
+
+	echo $errcode;
+	return -1;
+}
+
 // DO SOME AJAX STUFF HERE.
 if($ajax=='newpage')
 {
 	$title=$_POST['title'];
 	$blogtitle=$_POST['blogtitle'];
 	$blogtext=$_POST['blogtext'];
-	echo "NEWPAGE $title / $blogtitle";
+	
+	$newfilename=phpupload('file');
+	echo "FILE: $newfilename";
+//	if($newfilename==-1)
+//		return;
 }
 ?>
